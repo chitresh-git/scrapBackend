fb576c4fc90bca18d59adcccfe83af6b00045b38
chitresh mourya <chitreshmourya3@gmail.com>
2024-07-02 00:26:18 +0530

    comments

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-// const express = require('express');
-// const axios = require('axios');
-// const cors = require('cors');
 
-// const app = express();
-// const PORT = 5000;
-
-// app.use(cors());
-
-// app.get('/fetch-website', async (req, res) => {
-//   const { url } = req.query;
-
-//   try {
-//     const response = await axios.get(url);
-//     res.send(response.data);
-//   } catch (error) {
-//     console.error('Error fetching website:', error);
-//     res.status(500).send('Failed to fetch content.');
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 
 
 const express = require('express');
@@ -38,18 +15,27 @@ app.get('/fetch-website', async (req, res) => {
   const { url } = req.query;
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url); // fetching html content using provided url 
     const htmlContent = response.data;
+    
 
-    const dom = new JSDOM(htmlContent);
+    const dom = new JSDOM(htmlContent);   // jsdom will parse the html and creates DOM like environment 
     const document = dom.window.document;
 
     const cssLinks = [...document.querySelectorAll('link[rel="stylesheet"]')].map(link => link.href);
+    // fetching all the css links 
+    // queryselector will select all the elements with rel='stylesheel'
+    // now .map() method will take link attribute and return its link.href i.e. Url of css file 
 
     const cssPromises = cssLinks.map(cssUrl => axios.get(cssUrl).then(res => res.data).catch(err => ''));
-    const cssContents = await Promise.all(cssPromises);
+    // fetching data from csslinks 
+    // axios.get() send the http request to cssurl
+    // after that  .then() return data of reponse
 
-    res.send({ htmlContent, cssContents: cssContents.filter(Boolean) });
+    const cssContents = await Promise.all(cssPromises);
+    // this will wait until complete css data get sotred in csscontent
+
+    res.send({ htmlContent, cssContents: cssContents.filter(Boolean) }); // sending html and its css back to client 
   } catch (error) {
     console.error('Error fetching website:', error);
     res.status(500).send('Failed to fetch content.');
